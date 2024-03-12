0ddd32689a8341643d30fb7e83650dadbbed4927
makingtheimpact <34148804+makingtheimpact@users.noreply.github.com>
2024-03-12 05:51:53 +0000

    [doc] build from commit 542f7d0d7bd1a12e1884ba4a1508b40e2514d472

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2024.3.11+542f7d0d',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
